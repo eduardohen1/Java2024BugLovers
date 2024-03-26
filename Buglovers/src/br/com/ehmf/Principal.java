6cf305c2a01810b64f2fd5b194c66669a0edfc34
eduardohen1 <eduardohmferreira@gmail.com>
2024-03-26 09:02:57 -0300

    Aula 06_03_24 Iniciando com Java

---
+++ Buglovers/src/br/com/ehmf/Principal.java
@@ -0,0 +1,68 @@
+package br.com.ehmf;
+
+public class Principal {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Iniciando com o Java");
+		
+		System.out.println("--------------------");
+		
+		boolean condicao = true;
+		if(!condicao) { 
+			System.out.println("Condição verdadeira!");
+		} else { 
+			System.out.println("Condição falsa");
+		}
+		System.out.println("--------------------");
+		
+		// validar nota do aluno
+		int nota = 2;
+		if(nota >= 6) {
+			System.out.println("Passou de ano!");
+		} else if(nota <= 3) {
+			System.out.println("Reprovou!");
+		} else {
+			System.out.println("Recuperação....");
+		}
+		
+		
+		switch(nota) {
+			case 5:
+				System.out.println("Recuperação");
+				break;
+			case 3:
+				System.out.println("Reprovou");
+				break;
+			case 6:
+			case 7:
+			case 8:
+			case 9:
+			case 10:
+				System.out.println("Passou!");
+				break;
+			default:
+				System.out.println("Limbo");
+				break;
+		}
+		System.out.println("--------------------");
+		
+		// estruturas de repetição
+		// for; while
+		for(int x = 0; x <= 10; x++) {
+			System.out.print(x + ", ");
+		}
+		System.out.println("--------------------");
+		
+		int z = 0;
+		while(z <= 200) {
+			System.out.print(z);
+			if(z < 200) 
+				System.out.print(", ");
+			z++;
+		}
+		
+		
+	}
+
+}
